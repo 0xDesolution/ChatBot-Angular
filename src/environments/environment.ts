69041091710c3ef4d141ab6267ba6030a759806e
xiEdwin95 <52876536+xiEdwin95@users.noreply.github.com>
2020-01-22 17:58:49 +0800

    Init Completed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot:'ca28f8d898c14c15805d7e26139f93df'
+  }
 };
 
 /*
